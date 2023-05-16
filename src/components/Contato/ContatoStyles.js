124c92a60cd3e0acdf2694a4cb248b172831981f
Oseas Oliveira <126169147+OseOliv@users.noreply.github.com>
2023-05-15 22:52:54 -0300

    Update ContatoStyles.js

--- src/components/Contato/ContatoStyles.js
+++ src/components/Contato/ContatoStyles.js
@@ -42,6 +42,11 @@ export const ContatoTittleContainer = styled.div`
     height: 3px;
     margin-left: 20px;
     background-color: rgba(137, 43, 226, 0.3);
+    
+      @media screen and (max-width: 756px) {
+      width: 100px;
+  
+    }
 }
   
   @media screen and (max-width: 756px) {
