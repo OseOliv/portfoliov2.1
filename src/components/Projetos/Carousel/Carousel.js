efa0ecc3913bd6715683b58cada0e4f90858991f
Oseas Oliveira <126169147+OseOliv@users.noreply.github.com>
2023-06-02 14:05:53 -0300

    Update Carousel.js

--- src/components/Projetos/Carousel/Carousel.js
+++ src/components/Projetos/Carousel/Carousel.js
@@ -7,12 +7,14 @@ import img0 from "../../../assets/img/teste-projetos.png";
 import img1 from "../../../assets/img/teste-projetos1.png";
 import img2 from "../../../assets/img/teste-projetos2.png";
 import img3 from "../../../assets/img/teste-projetos3.png";
-import img4 from "../../../assets/img/teste-projetos5.png"
+import img4 from "../../../assets/img/teste-projetos5.png";
+import img5 from "../../../assets/img/teste-projetos5.png";
 
 import { CarouselContainer } from "../ProjetosStyles";
 
 const projetosImg =  [
 { img: img0, link: "https://github.com/OseOliv/portfolio-v1" },
+{ img: img5, link: "https://github.com/OseOliv/op-memory-game" },
 { img: img1, link: "https://github.com/OseOliv/Exemple-Coffee-Shop-WebSite-BootStrap" },
 { img: img2, link: "https://github.com/OseOliv/Exercicio-1-CSS-DIO" },
 { img: img3, link: "https://github.com/OseOliv/Pokedex" },
